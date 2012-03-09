54face99552e39fa4b2067d5474f79c64c7ad580
Ardi Soebrata <ardissoebrata@gmail.com>
2012-03-09 13:10:38 +0700

    Add pagination config.

---
+++ application/config/pagination.php
@@ -0,0 +1,33 @@
+<?php
+
+$config['num_links']			= 2;
+$config['use_page_numbers']		= FALSE;
+$config['page_query_string']	= FALSE;
+$config['uri_segment']			= 4;
+
+$config['full_tag_open']	= '<div class="pagination pagination-right"><ul>';
+$config['full_tag_close']	= '</ul></div>';
+
+$config['first_link']		= 'First';
+$config['first_link_open']	= '<li>';
+$config['first_list_close']	= '</li>';
+
+$config['last_link']		= 'Last';
+$config['last_link_open']	= '<li>';
+$config['last_link_close']	= '</li>';
+
+$config['next_link']		= '&gt;';
+$config['next_tag_open']	= '<li>';
+$config['next_tag_close']	= '</li>';
+
+$config['prev_link']		= '&lt;';
+$config['prev_tag_open']	= '<li>';
+$config['prev_tag_close']	= '</li>';
+
+$config['cur_tag_open']		= '<li class="active"><a href="#">';
+$config['cur_tag_close']	= '</a></li>';
+
+$config['num_tag_open']		= '<li>';
+$config['num_tag_close']	= '</li>';
+
+$config['display_pages']	= TRUE;
