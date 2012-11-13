41fbbb00649ec5d02cd10afeb601862a05edfa93
Ardi Soebrata <ardissoebrata@gmail.com>
2012-11-13 17:32:58 +0700

    Autodetect base_url.

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ci-beam/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
